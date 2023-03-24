6e8671f7418cf1745d1c6813420c896dc506562c
Franisz <47374092+Franisz@users.noreply.github.com>
2023-03-24 18:07:06 +0100

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const dotenv = require("dotenv").config();
 const fs = require("fs");
-const { color } = require("console-log-colors");
 
 function getFiles(dir, files_) {
   files_ = files_ || [];
@@ -16,104 +15,115 @@ function getFiles(dir, files_) {
   return files_;
 }
 
-function getCurioTracker(ddPath) {
-  const paths = getFiles(ddPath);
+function addQuestCurio(path, curioList) {
+  var { goals } = require(path);
 
-  // all vanilla items that can be used on curios from the inventory
-  // id: type
-  var usableItems = {
-    shovel: "supply",
-    firewood: "supply",
-    bandage: "supply",
-    antivenom: "supply",
-    skeleton_key: "supply",
-    medicinal_herbs: "supply",
-    torch: "supply",
-    holy_water: "supply",
-    dog_treats: "supply",
-    laudanum: "supply",
-    the_blood: "estate", // cc, the_blood#estate
-    the_cure: "estate", // cc
-    snake_scale: "estate", // shieldbreaker
-    spice: "supply", // com, shard dust
-    provision: "provision", // some items have special type and empty id
-  };
+  for (const quest of goals) {
+    if (quest.type != "gather" && quest.type != "activate") continue;
 
-  var curioList = {};
-  var tempCurio = {
-    prop_name_id: "",
-    item_type: "",
-    interactions: [],
-  };
+    var curioName = `${quest.data.curio_name}_quest_${quest.type}`;
+    if (quest.id.startsWith("inventory_activate_")) curioName += "_with";
+
+    var usableItem =
+      quest.starting_items && quest.starting_items[0]
+        ? quest.starting_items[0]
+        : {};
+
+    curioList[curioName] = {
+      interactions: usableItem,
+    };
+  }
+
+  return curioList;
+}
+
+function addLibraryCurio(path, curioList) {
+  const inputFile = fs.readFileSync(path, "UTF8").toString();
+  const fileLines = inputFile.split("\n");
 
   var curioHeaderLine = 0;
-  for (let i = 0; i < paths.length; i++) {
-    // const fileName = paths[i].substr(paths[i].lastIndexOf("\\") + 1);
-    if (!paths[i].endsWith("curio_type_library.csv")) continue;
-    else console.log(`Input: ${paths[i]}`);
+  var lastCurioId = "";
+  for (let i = 0; i < fileLines.length; i++) {
+    // Single csv line
+    const values = fileLines[i].split(",");
+    if (values.length < 16) continue;
 
-    const inputFile = fs.readFileSync(paths[i], "UTF8").toString();
-    const fileLines = inputFile.split("\n");
+    // i == header line of new curio
+    if (values[2].length && values[2] == "ID STRING") {
+      curioHeaderLine = i;
+    }
 
-    for (let j = 0; j < fileLines.length; j++) {
-      // Single csv line
-      const values = fileLines[j].split(",");
-      if (values.length < 16) continue;
+    // values[2] - curio id
+    if (i == curioHeaderLine + 1 && values[2].length) {
+      lastCurioId = values[2];
 
-      // j == header line of new curio
-      if (values[2].length && values[2] == "ID STRING") {
-        if (tempCurio.prop_name_id.length) {
-          curioList[tempCurio.prop_name_id] = {
-            interactions: tempCurio.interactions,
-          };
+      curioList[lastCurioId] = {
+        interactions: {},
+      };
+    }
 
-          tempCurio = {};
-        }
+    // possible item interactions with the curio
+    // values[4] - usable item id
+    // values[16] - id of outcome image for the tracker
+    if (i >= curioHeaderLine + 11 && values[4].length && values[16].length) {
+      const item_values = values[4].split("#");
+      const item_id = item_values[0] || item_values[1] || "";
 
-        curioHeaderLine = j;
+      // adding unknown items to usableItems list
+      if (!usableItems[item_id]) {
+        usableItems[item_id] = item_values[1] || "supply";
+        console.log(
+          `New usable detected: { ${item_id}: "${usableItems[item_id]}" }`
+        );
       }
 
-      // values[2] - curio id
-      if (j == curioHeaderLine + 1 && values[2].length) {
-        tempCurio.prop_name_id = values[2];
-        tempCurio.interactions = {};
-      }
+      const item_type = item_values[1] || usableItems[item_id];
 
-      // possible item interactions with the curio
-      // values[4] - usable item id
-      // values[16] - id of outcome image for the tracker
-      if (j >= curioHeaderLine + 11 && values[4].length && values[16].length) {
-        const item_values = values[4].split("#");
-        const item_id = item_values[0] || item_values[1] || "";
-
-        if (item_values[0] == item_values[1]) console.log(item_id);
-
-        if (!usableItems[item_id]) {
-          usableItems[item_id] = item_values[1] || "supply";
-          console.log(
-            color.yellow(
-              `New usable detected: { ${item_id}: "${usableItems[item_id]}" }`
-            )
-          );
-        }
-
-        const item_type = item_values[1] || usableItems[item_id];
-
-        tempCurio.interactions[item_id] = {
-          item_type: item_type,
-          curio_tracker_id: values[16],
-        };
-      }
+      curioList[lastCurioId].interactions[item_id] = {
+        item_type: item_type,
+        curio_tracker_id: values[16],
+      };
     }
   }
 
+  return curioList;
+}
+
+function getCurioList(ddPath) {
+  // array of paths to all files in Darkest Dungeon folder
+  const paths = getFiles(ddPath);
+
+  var curioList = {};
+  for (const path of paths) {
+    if (path.endsWith("quest.types.json")) {
+      console.log(`Input: ${path}`);
+      addQuestCurio(path, curioList);
+      continue;
+    }
+
+    if (path.endsWith("curio_type_library.csv")) {
+      console.log(`Input: ${path}`);
+      addLibraryCurio(path, curioList);
+      continue;
+    }
+  }
+
+  return curioList;
+}
+
+// all vanilla quest curios with their respective quest items
+// quest curio are either activated without any item or with a specific quest item
+// items of quest_item type don't have quest tracker icon, so they aren't added to usable items list
+// quest curios don't have to appear in curio libraries, but if they do, everything will still work fine
+
+function getCurioTracker(curioList) {
   var curio_tracker = {
     // __revision_dont_touch: 1683488768,
     base_root: { version: 1, tracked_results: {} },
   };
 
   var num = 0;
-  for (const curio in curioList) {
+  for (const curio in curioList)
     for (const usable in usableItems) {
       const type_hash = curioList[curio].item_type || usableItems[usable];
       const tracker_id =
@@ -128,18 +138,24 @@ function getCurioTracker(ddPath) {
         curio_tracker_id: tracker_id,
       };
     }
-  }
 
   return curio_tracker;
 }
 
-const curioTracker = getCurioTracker(process.env.DDPATH);
+// all vanilla items that can be used on curios from the inventory
+// id: type
+const usableItems = require("./data/usable_items.json");
+
+// list of all curios and their interactable items if there are any
+const curioList = getCurioList(process.env.DDPATH);
+
+const curioTracker = getCurioTracker(curioList);
 const curioTrackerName = "curio_tracker.json";
 fs.writeFile(
   `bin\\${curioTrackerName}`,
   JSON.stringify(curioTracker, null, 2),
   (err) => {
     if (err) throw err;
-    console.log(`Output: \\bin\\${color.greenBright(curioTrackerName)}`);
+    console.log(`Output: \\bin\\${curioTrackerName}`);
   }
 );
